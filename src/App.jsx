e6ac1d1804ad9b6456817fc89de89c32011fdb01
Bohdan <6alfabravo9@gmail.com>
2023-06-21 10:58:33 +0300

    prepare data and make layoult

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import categoriesFromServer from './api/categories';
-// import productsFromServer from './api/products';
+import usersFromServer from './api/users';
+import categoriesFromServer from './api/categories';
+import productsFromServer from './api/products';
 
-// const products = productsFromServer.map((product) => {
-//   const category = null; // find by product.categoryId
-//   const user = null; // find by category.ownerId
+const getProductCategory = categoryId => categoriesFromServer
+  .find(category => category.id === categoryId) || null;
 
-//   return null;
-// });
+const getUser = ownerId => usersFromServer
+  .find(user => user.id === ownerId) || null;
+
+const products = productsFromServer.map((product) => {
+  const { categoryId } = product;
+
+  const category = getProductCategory(categoryId);
+  const user = getUser(category.ownerId);
+
+  return {
+    ...product,
+    category,
+    user,
+  };
+});
 
 export const App = () => (
   <div className="section">
     <div className="container">
       <h1 className="title">Product Categories</h1>
-
+      {products[0].user.name}
       <div className="block">
         <nav className="panel">
           <p className="panel-heading">Filters</p>
@@ -192,53 +205,27 @@ export const App = () => (
           </thead>
 
           <tbody>
-            <tr data-cy="Product">
-              <td className="has-text-weight-bold" data-cy="ProductId">
-                1
-              </td>
+            {products.map(product => (
+              <tr data-cy="Product">
+                <td className="has-text-weight-bold" data-cy="ProductId">
+                  {product.id}
+                </td>
 
-              <td data-cy="ProductName">Milk</td>
-              <td data-cy="ProductCategory">🍺 - Drinks</td>
+                <td data-cy="ProductName">
+                  {product.name}
+                </td>
+                <td data-cy="ProductCategory">
+                  {`${product.category.icon} - ${product.category.title}`}
+                </td>
 
-              <td
-                data-cy="ProductUser"
-                className="has-text-link"
-              >
-                Max
-              </td>
-            </tr>
-
-            <tr data-cy="Product">
-              <td className="has-text-weight-bold" data-cy="ProductId">
-                2
-              </td>
-
-              <td data-cy="ProductName">Bread</td>
-              <td data-cy="ProductCategory">🍞 - Grocery</td>
-
-              <td
-                data-cy="ProductUser"
-                className="has-text-danger"
-              >
-                Anna
-              </td>
-            </tr>
-
-            <tr data-cy="Product">
-              <td className="has-text-weight-bold" data-cy="ProductId">
-                3
-              </td>
-
-              <td data-cy="ProductName">iPhone</td>
-              <td data-cy="ProductCategory">💻 - Electronics</td>
-
-              <td
-                data-cy="ProductUser"
-                className="has-text-link"
-              >
-                Roma
-              </td>
-            </tr>
+                <td
+                  data-cy="ProductUser"
+                  className="has-text-link"
+                >
+                  {product.user.name}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
